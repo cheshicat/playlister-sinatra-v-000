e823d82cd5eac4357eba06b12c241fd6a865ec24
Allison Howard <alliemhow@gmail.com>
2019-05-21 10:02:32 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,13 +4,13 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
    @song = Song.find_by_slug(params[:slug])
    erb :'songs/show'
-  end
-
-  get '/songs/new' do
-    erb :'/songs/new'
   end
 
   post '/songs' do
@@ -20,5 +20,22 @@ class SongsController < ApplicationController
     @song.save
 
     erb :'songs/show', locals: {message: "Successfully created song."}
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+
+    erb :'songs/edit'
+  end
+
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+
+    @song.update(params[:song])
+
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+
+    erb :'songs/show', locals: {message: "Song successfully updated."}
   end
 end
