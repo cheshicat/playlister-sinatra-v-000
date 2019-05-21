807f62b80e68839287a899f83fc3f52e3d935496
Allison Howard <alliemhow@gmail.com>
2019-05-21 09:43:23 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,13 @@ class SongsController < ApplicationController
     @songs = Song.all
     erb :'/songs/index'
   end
+
+  post '/songs' do
+    @song = Song.create(:name => params["Name"])
+    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+
+    erb :'songs/show', locals: {message: "Successfully created song."}
+  end
 end
